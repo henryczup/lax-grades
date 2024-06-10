4af9ccefaf555bd7c7358dd6a2628598c09ebdbf
henryczup <140286297+henryczup@users.noreply.github.com>
2024-06-10 11:12:50 -0500

    Title update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import NavBar from "@/components/component/shared-components/navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "LaxGrades - Explore UWL Grade Distributions",
+  title: "LAXGRADES - Explore UWL Grade Distributions",
   description: "Discover grade distributions for classes at the University of Wisconsin-La Crosse. Search and compare historical grade data, GPAs, and pass rates for courses across various semesters and instructors.",
   keywords: "UWL, University of Wisconsin-La Crosse, grade distributions, GPA, pass rates, courses, classes, instructors, semesters",
 };
